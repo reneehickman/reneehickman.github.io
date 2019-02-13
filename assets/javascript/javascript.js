9a36ecef1e2f8ce5fac86ff5b002c982e6a9169f
Renee Hickman <renee.hickman@outlook.com>
2019-02-13 15:34:00 -0600

    html/css

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,8 +2,7 @@ $(function() {
     $('a[href*="#"]').click(function() {
       var target = $(this.hash);
       if (target.length) {
-        $("html, body").animate(
-          {
+        $("html, body").animate( {
             scrollTop: target.offset().top
           },
           1000
@@ -11,3 +10,9 @@ $(function() {
       }
     });
   });
+
+  $('#sideNav a').click(function(e) {
+    e.preventDefault();
+    $('#sideNav a').removeClass('active');
+    $(this).addClass('active');
+});
