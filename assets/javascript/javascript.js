7801e99fb057d1707b8248615d3591904bfdfad3
reneecosmic <renee.hickman@outlook.com>
2019-02-14 08:18:47 -0600

    html/styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,6 @@
 $(function() {
     $('a[href*="#"]').click(function() {
+      $('#sideNav a').removeClass('active');
       var target = $(this.hash);
       if (target.length) {
         $("html, body").animate( {
@@ -16,3 +17,33 @@ $(function() {
     $('#sideNav a').removeClass('active');
     $(this).addClass('active');
 });
+
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //     if (target.length) {
+  //       $('html, body').animate({
+  //         scrollTop: (target.offset().top)
+  //       }, 1000, "easeInOutExpo");
+  //       return false;
+  //     }
+  //   }
+  // });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+})(jQuery); // End of use strict
